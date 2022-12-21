7158f90a06e4c5c4853a88c877a1a62759cb2bba
samwise00 <sam.sandoval.00@gmail.com>
2022-12-21 01:02:35 -0500

    complete thegraph integration

---
+++ tests/limitless-ocean-utils.ts
@@ -0,0 +1,139 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  ItemBought,
+  ItemCanceled,
+  ItemListed,
+  ListingUpdated
+} from "../generated/LimitlessOcean/LimitlessOcean"
+
+export function createItemBoughtEvent(
+  seller: Address,
+  buyer: Address,
+  nftAddress: Address,
+  tokenId: BigInt,
+  price: BigInt
+): ItemBought {
+  let itemBoughtEvent = changetype<ItemBought>(newMockEvent())
+
+  itemBoughtEvent.parameters = new Array()
+
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam("buyer", ethereum.Value.fromAddress(buyer))
+  )
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return itemBoughtEvent
+}
+
+export function createItemCanceledEvent(
+  nftAddress: Address,
+  tokenId: BigInt,
+  owner: Address
+): ItemCanceled {
+  let itemCanceledEvent = changetype<ItemCanceled>(newMockEvent())
+
+  itemCanceledEvent.parameters = new Array()
+
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+
+  return itemCanceledEvent
+}
+
+export function createItemListedEvent(
+  seller: Address,
+  nftAddress: Address,
+  tokenId: BigInt,
+  price: BigInt
+): ItemListed {
+  let itemListedEvent = changetype<ItemListed>(newMockEvent())
+
+  itemListedEvent.parameters = new Array()
+
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return itemListedEvent
+}
+
+export function createListingUpdatedEvent(
+  seller: Address,
+  nftAddress: Address,
+  tokenId: BigInt,
+  newPrice: BigInt
+): ListingUpdated {
+  let listingUpdatedEvent = changetype<ListingUpdated>(newMockEvent())
+
+  listingUpdatedEvent.parameters = new Array()
+
+  listingUpdatedEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  listingUpdatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  listingUpdatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  listingUpdatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "newPrice",
+      ethereum.Value.fromUnsignedBigInt(newPrice)
+    )
+  )
+
+  return listingUpdatedEvent
+}
